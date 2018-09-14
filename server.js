d4e78d2c533175d9de7d61f8dacb8af4334aa97f
richjimdev <richjimdev@gmail.com>
2018-09-14 16:57:18 -0700

    fixed bug

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const express = require('express');
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-const conString = 'postgress://rich:6641@localhost:5432/kilovolt';
+const conString = process.env.DATABASE_URL || 'postgres://rich:6641@localhost:5432/kilovolt';
 const client = new pg.Client(conString);
 client.connect();
 client.on('error', error => {
